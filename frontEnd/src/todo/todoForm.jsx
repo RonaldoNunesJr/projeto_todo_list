af7ff756287428c2d4947c84ae104e6e910a5d3c
Ronaldo Nunes jr <ronaldo.nunes.jr@gmail.com>
2018-03-06 17:20:33 -0300

    ajuste na regra de exibicao no botao de pause

--- frontEnd/src/todo/todoForm.jsx
+++ frontEnd/src/todo/todoForm.jsx
@@ -25,7 +25,7 @@ export default props => {
                     value={props.description}></textarea>
             </Grid>
             <Grid cols='12 3 2'>
-                <IconButton style='primary' icon='plus'
+                <IconButton style='primary' icon='plus' 
                     onClick={props.handleAdd}></IconButton>
                 <IconButton style='info' icon='search'
                     onClick={props.handleSearch}></IconButton>
--- frontEnd/src/todo/todoList.jsx
+++ frontEnd/src/todo/todoList.jsx
@@ -6,30 +6,35 @@ export default props => {
     const renderRows = () => {
         const list = props.list || []
         
-        return list.map(todo => (
-            <tr key={todo._id}>
-                <td className={todo.done ? 'markedAsDone':''}>
-                <strong>{todo.title}</strong><br />
-                <small>{todo.description}</small>
-                </td>
-                <td>
-                    <IconButton style='succes' icon='check' hide={todo.done && !todo.pauseStatus}
-                    onClick={() => props.handleMarkAsDone(todo)}></IconButton>
 
-                    <IconButton style='success' icon='pausse-circle' hide={!todo.pauseStatus}
-                    onClick={() => props.handleMarkAsPause(todo)}></IconButton>
-                    
-                    <IconButton style='success' icon='play-circle' hide={todo.pauseStatus}
-                    onClick={() => props.handleMarkAsResume(todo)}></IconButton>
-                    
-                    <IconButton style='warning' icon='undo' hide={todo.done && !todo.pauseStatus}
-                    onClick={() => props.handleMarkAsPending(todo)}></IconButton>
-                    
-                    <IconButton style='danger' icon='trash-o' hide={!todo.done && !todo.pauseStatus}
-                    onClick={() => props.handleRemove(todo)}></IconButton>
-                </td>
-            </tr>
-        ))
+        return list.map(todo => {
+            const showPause = todo.done && !todo.pauseStatus
+            
+            return (
+                <tr key={todo._id}>
+                    <td className={todo.done ? 'markedAsDone':''}>
+                    <strong>{todo.title}</strong><br />
+                    <small>{todo.description}</small>
+                    </td>
+                    <td>
+                        <IconButton style='succes' icon='check' hide={!showPause}
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+
+                        <IconButton style='success' icon='pause-circle' hide={!todo.pauseStatus}
+                        onClick={() => props.handleMarkAsPause(todo)}></IconButton>
+                        
+                        <IconButton style='success' icon='play-circle' hide={todo.pauseStatus}
+                        onClick={() => props.handleMarkAsResume(todo)}></IconButton>
+                        
+                        <IconButton style='warning' icon='undo' hide={todo.done && !todo.pauseStatus}
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                        
+                        <IconButton style='danger' icon='trash-o' hide={todo.done && !todo.pauseStatus}
+                        onClick={() => props.handleRemove(todo)}></IconButton>
+                    </td>
+                </tr>
+            )
+        })
     }   
 
     return (
